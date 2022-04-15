ae5bc1a038676b7f58d15d3eff4e2b88da967ff2
Loïc LEFEVRE <loic.lefevre@gmail.com>
2022-04-15 23:23:28 +0200

    Create buildContainerImage.sh

---
+++ buildContainerImage.sh
@@ -0,0 +1,76 @@
+#!/bin/bash
+# Since: April, 2022
+# Author: loiclefevre
+# Name: buildContainerImage.sh
+# Description: Build a Container image
+#
+# Copyright 2022 Loïc Lefèvre
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+
+# Exit on errors
+# Great explanation on https://vaneyckt.io/posts/safer_bash_scripts_with_set_euxo_pipefail/
+set -Eeuo pipefail
+
+VERSION="ATPSFree19"
+IMAGE_NAME="loiclefevre/oracle-atps"
+
+function usage() {
+    cat << EOF
+Usage: buildContainerImage.sh [-v version] [-o] [container build option]
+Builds a container image for Oracle Autonomous Database Shared infrastructure.
+Parameters:
+   -v: version of Oracle Autonomous Database to build
+       Choose one of: ATPSFree19, ATPSFree21
+   -o: passes on container build option
+Apache License, Version 2.0
+Copyright (c) 2022 Loïc Lefèvre
+EOF
+
+}
+
+while getopts "v:o:" optname; do
+  case "${optname}" in
+    "h")
+      usage
+      exit 0;
+      ;;
+    "v")
+      VERSION="${OPTARG}"
+      ;;
+    "o")
+      eval "BUILD_OPTS=(${OPTARG})"
+      ;;
+    "?")
+      usage;
+      exit 1;
+      ;;
+    *)
+    # Should not occur
+      echo "Unknown error while processing options inside buildContainerImage.sh"
+      ;;
+  esac;
+done;
+
+IMAGE_NAME="${IMAGE_NAME}:${VERSION}"
+
+echo "BUILDER: building image $IMAGE_NAME"
+
+BUILD_START_TMS=$(date '+%s')
+
+buildah bud -f Dockerfile."${VERSION//./}" -t "${IMAGE_NAME}"
+
+BUILD_END_TMS=$(date '+%s')
+BUILD_DURATION=$(( BUILD_END_TMS - BUILD_START_TMS ))
+
+echo "Build of container image ${IMAGE_NAME} completed in ${BUILD_DURATION} seconds."
