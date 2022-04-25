5d7e846c8b3f6d324561c43b1681dc5414421dd6
LLEFEVRE <loic.lefevre@gmail.com>
2022-04-25 15:43:18 +0200

    HTTP for current IP address validation

--- src/main/java/com/oracle/dragonlite/Main.java
+++ src/main/java/com/oracle/dragonlite/Main.java
@@ -198,7 +198,7 @@ public class Main {
 	private String retrieveCurrentIPAddress() {
 		try {
 			final HttpRequest request = HttpRequest.newBuilder()
-					.uri(new URI("checkip.dyndns.org"))
+					.uri(new URI("http://checkip.dyndns.org"))
 					.GET()
 					.build();
 
