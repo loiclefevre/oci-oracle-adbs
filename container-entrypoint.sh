1588dc64b7c2f302d67a7bdf68ad8235820190cc
LLEFEVRE <loic.lefevre@gmail.com>
2022-04-29 22:47:10 +0200

    Docker integration

--- container-entrypoint.sh
+++ container-entrypoint.sh
@@ -157,8 +157,6 @@ trap stop_database SIGINT SIGTERM
 
 echo `date +"%H:%M:%S.000"`" INFO  🐳 Container - starting up..."
 
-echo ${ACTION} ${DATABASE_NAME} ${SYSTEM_PASSWORD} ${USER} ${USER_PASSWORD}
-
 # Setup all required environment variables
 #setup_env_vars
 
